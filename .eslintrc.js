f1493c9ad7743ec4c5d6768b435d61ef779ab90f
Reutovich, Yuliya <y.reutovich@itransition.com>
2023-07-03 16:36:22 +0300

    feat: fix escipnfig, tsconfig; add server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,40 @@
+const { resolve } = require('path');
+
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
+    browser: false,
+    node: true
   },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    extraFileExtensions: ['.ts'],
+    parser: '@typescript-eslint/parser',
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
     ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+    sourceType: 'module'
+  },  
+  plugins: [
+    "@typescript-eslint",
+    'prettier'
+  ],
   rules: {
     'no-console': 'off',
     'import/prefer-default-export': 'off',
     'class-methods-use-this': 'off',
     'import/extensions': 'off',
   },
+  settings: {
+    "import/resolver": {
+      typescript: {}
+    }
+  }
+  
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { createServer } from "http";
+
+import { PORT } from './constants/constants.ts'
+
+
+const server = createServer();
+
+server.listen(PORT)
+
+
+server.on('request', (request, res) => {
+  console.log(request.url, res)
+});
